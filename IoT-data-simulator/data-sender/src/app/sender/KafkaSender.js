1d9042d8f986345ed5a445c477f730a5346ff260
max <maxr>
2024-06-23 13:18:25 -0400

    added files

--- IoT-data-simulator/data-sender/src/app/sender/KafkaSender.js
+++ IoT-data-simulator/data-sender/src/app/sender/KafkaSender.js
@@ -1,27 +1,21 @@
-let logger = require('../common/log');
-let CommonSender = require('./CommonSender');
-let Constants = require('../common/Constants');
-let SecurityBuilder = require('../security/SecurityBuilder');
-let utils = require('../common/senderUtils');
-let entitySerializer = require('../serializer/EntitySerializer');
-let kafka = require('kafka-node');
-let Producer = kafka.Producer;
-let KeyedMessage = kafka.KeyedMessage;
-let KafkaClient = kafka.KafkaClient;
+const logger = require('../common/log');
+const CommonSender = require('./CommonSender');
+const Constants = require('../common/Constants');
+const SecurityBuilder = require('../security/SecurityBuilder');
+const utils = require('../common/senderUtils');
+const entitySerializer = require('../serializer/EntitySerializer');
+const kafka = require('kafka-node');
 
+const { Producer, KeyedMessage, KafkaClient } = kafka;
 
 class KafkaSender extends CommonSender {
-
-    /**
-     *
-     */
     constructor() {
         super();
         this.keyFunctions = new Map();
     }
 
     /**
-     *
+     * Get the AMQP queue
      * @returns {string}
      */
     getAmqpQueue() {
@@ -29,21 +23,20 @@ class KafkaSender extends CommonSender {
     }
 
     /**
-     *
-     * @param sessionId
-     * @param target
-     * @param payload
-     * @returns {Promise.<void>}
+     * Send payload to Kafka target system
+     * @param {string} sessionId
+     * @param {Object} target
+     * @param {string} payload
+     * @returns {Promise<void>}
      */
     async sendPayload(sessionId, target, payload) {
-
         logger.debug(">>> Kafka send payload: processing kafka send payload.");
 
-        let connectionMapKey = JSON.stringify(target);
+        const connectionMapKey = JSON.stringify(target);
         let clientConnection = this.clientConnections.get(connectionMapKey);
 
         if (!clientConnection) {
-            clientConnection = await this.getClientConnection(target);
+            clientConnection = await this.getClientConnection(target, connectionMapKey);
             this.clientConnections.set(connectionMapKey, clientConnection);
         }
 
@@ -52,19 +45,18 @@ class KafkaSender extends CommonSender {
     }
 
     /**
-     *
-     * @param target
-     * @returns {Promise.<void>}
+     * Get client connection
+     * @param {Object} target
+     * @param {string} connectionMapKey
+     * @returns {Promise<Producer>}
      */
     async getClientConnection(target, connectionMapKey) {
-
         logger.debug(`>>> Opening kafka connection to target: ${JSON.stringify(target)}`);
 
-        let client = await this.buildKafkaClient(target);
-        let producer = new Producer(client, { requireAcks: 1 });
+        const client = await this.buildKafkaClient(target);
+        const producer = new Producer(client, { requireAcks: 1 });
 
         return new Promise((resolve, reject) => {
-
             producer.on("ready", () => {
                 logger.debug(`>>> Kafka producer connection has been opened.`);
                 resolve(producer);
@@ -79,57 +71,54 @@ class KafkaSender extends CommonSender {
     }
 
     /**
-     *
-     * @param target
-     * @param security
+     * Build Kafka client
+     * @param {Object} target
      * @returns {Promise<KafkaClient>}
      */
     async buildKafkaClient(target) {
+        const security = await this.getSecurityData(target.security);
+        const clientOptions = {
+            kafkaHost: target.url,
+            connectTimeout: 5000,
+            requestTimeout: 5000,
+            sslOptions: security || undefined,
+        };
 
-        let security = await this.getSecurityData(target.security);
-        if (security) {
-            return new KafkaClient({kafkaHost:target.url, connectTimeout: 5000, requestTimeout: 5000, sslOptions: security});
-        } else {
-            return new KafkaClient({kafkaHost:target.url, connectTimeout: 5000, requestTimeout: 5000});
-        }
+        return new KafkaClient(clientOptions);
     }
 
     /**
-     *
-     * @param target
-     * @param payload
-     * @param clientConnection
+     * Send payload to Kafka
+     * @param {Object} target
+     * @param {string} payload
+     * @param {Producer} clientConnection
      * @returns {Promise}
      */
     async sendKafkaPayload(target, payload, clientConnection) {
-
-        let key = this.getKafkaKey(target, payload);
-        let message = await this.buildKafkaMessage(target, payload, key);
+        const key = this.getKafkaKey(target, payload);
+        const message = await this.buildKafkaMessage(target, payload, key);
 
         return new Promise((resolve, reject) => {
-
-            clientConnection.send([{topic: target.topic, messages: message}],
-
-                function(err, result) {
-                    if(err){
+            clientConnection.send([{ topic: target.topic, messages: message }],
+                (err, result) => {
+                    if (err) {
                         reject(err);
                     } else {
                         resolve(result);
                     }
                 }
-            )
-        })
+            );
+        });
     }
 
     /**
-     *
-     * @param target
-     * @param payload
-     * @param key
-     * @returns {Promise<void>}
+     * Build Kafka message
+     * @param {Object} target
+     * @param {string} payload
+     * @param {string} key
+     * @returns {Promise<KeyedMessage|string>}
      */
     async buildKafkaMessage(target, payload, key) {
-
         if (key) {
             return await this.buildKeyedMessage(target, payload, key);
         } else {
@@ -138,14 +127,13 @@ class KafkaSender extends CommonSender {
     }
 
     /**
-     *
-     * @param target
-     * @param payload
-     * @param key
-     * @returns {Promise<*>}
+     * Build keyed message for Kafka
+     * @param {Object} target
+     * @param {string} payload
+     * @param {string} key
+     * @returns {Promise<KeyedMessage>}
      */
     async buildKeyedMessage(target, payload, key) {
-
         if (target.keySerializer) {
             return await this.getEncryptedKeyedMessage(target, payload, key);
         } else if (target.messageSerializer || utils.isStringValue(payload)) {
@@ -156,13 +144,12 @@ class KafkaSender extends CommonSender {
     }
 
     /**
-     *
-     * @param target
-     * @param payload
-     * @returns {Promise<*>}
+     * Build message for Kafka
+     * @param {Object} target
+     * @param {string} payload
+     * @returns {string}
      */
     buildMessage(target, payload) {
-
         if (target.messageSerializer || utils.isStringValue(payload)) {
             return payload;
         } else {
@@ -171,19 +158,16 @@ class KafkaSender extends CommonSender {
     }
 
     /**
-     *
-     * @param target
-     * @param payload
-     * @returns {*}
+     * Get Kafka key
+     * @param {Object} target
+     * @param {string} payload
+     * @returns {string}
      */
     getKafkaKey(target, payload) {
-
         if (target.keyFunction) {
-
             let keyFunction = this.keyFunctions.get(target.keyFunction);
             if (!keyFunction) {
-
-                keyFunction = eval('(' + target.keyFunction + ')');
+                keyFunction = eval(`(${target.keyFunction})`);
                 this.keyFunctions.set(target.keyFunction, keyFunction);
             }
 
@@ -192,68 +176,59 @@ class KafkaSender extends CommonSender {
     }
 
     /**
-     *
-     * @param target
-     * @param payload
-     * @param kafkaKey
-     * @returns {*}
+     * Get encrypted keyed message
+     * @param {Object} target
+     * @param {string} payload
+     * @param {string} kafkaKey
+     * @returns {Promise<KeyedMessage>}
      */
     async getEncryptedKeyedMessage(target, payload, kafkaKey) {
-
         logger.debug(">>> Getting keyed message using protobuf");
-        let encryptedKey = await entitySerializer.serialize(target, Constants.KEY_SERIALIZER, kafkaKey);
+        const encryptedKey = await entitySerializer.serialize(target, Constants.KEY_SERIALIZER, kafkaKey);
         return new KeyedMessage(encryptedKey, payload);
     }
 
-
     /**
-     *
-     * @param security
-     * @returns {*}
+     * Get security data
+     * @param {Object} security
+     * @returns {Promise<Object>}
      */
     async getSecurityData(security = {}) {
-
         return new Promise((resolve, reject) => {
-
             if (!security.type) {
                 return resolve(null);
             }
 
-            switch(security.type) {
-
-                case Constants.SECURITY_CERTIFICATE: {
-
-                    resolve (SecurityBuilder.getCertificates(security));
+            switch (security.type) {
+                case Constants.SECURITY_CERTIFICATE:
+                    resolve(SecurityBuilder.getCertificates(security));
                     break;
-                }
-
-                default: {
+                default:
                     reject(new Error(`${security.type} doesn't supported`));
-                }
             }
         });
     }
 
     /**
-     *
-     * @param sessionId
-     * @param target
+     * Handle session failure
+     * @param {string} sessionId
+     * @param {Object} target
      */
     handleSessionFailure(sessionId, target) {
         this.closeConnection(sessionId, target);
     }
 
     /**
-     *
+     * Close connection
+     * @param {string} sessionId
+     * @param {Object} target
      */
-    closeConnection() {
-
-        let connectionMapKey = JSON.stringify(target);
-        let clientConnection = this.clientConnections.get(connectionMapKey);
+    closeConnection(sessionId, target) {
+        const connectionMapKey = JSON.stringify(target);
+        const clientConnection = this.clientConnections.get(connectionMapKey);
 
         if (clientConnection) {
-
-            clientConnection.client.close();
+            clientConnection.close();
             logger.debug(`>>> Removing client connection for session ${sessionId} from cache.`);
             this.clientConnections.delete(connectionMapKey);
         }
